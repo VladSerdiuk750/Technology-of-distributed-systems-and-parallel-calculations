5711848f8b3fccf3ff238153a6d2d0591cb7db37
Vultys750 <fan.of.Dnipro@gmail.com>
2019-03-04 12:26:57 +0200

    example 1-8
    Delegates and events

---
+++ TDSPC/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDSPC
+{
+    delegate void strMod(ref string stx);
+
+    class DelegateTest
+    {
+        public void replaceSpaces(ref string a)
+        {
+            Console.WriteLine("Замена ");
+            a = a.Replace(' ', '-');
+        }
+
+        public void removeSpaces(ref string a)
+        {
+            string temp = "";
+            Console.WriteLine("Удаление проблелов.");
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] != ' ') temp += a[i];
+            a = temp;
+        }
+        public void reverse(ref string a)
+        {
+            string temp = "";
+            Console.WriteLine("Реверсирование строки.");
+            for (int j = 0, i = a.Length - 1; i >= 0; i--, j++)
+                temp += a[i];
+            a = temp;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "   Пример №1";
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            string str = "Это простой тест.";
+            var list = new List<strMod>()
+            {
+                new DelegateTest().replaceSpaces,
+                new DelegateTest().reverse,
+                new DelegateTest().removeSpaces
+            };
+            var strOp = list[0];
+            strOp += list[1];
+            strOp(ref str);
+            Console.WriteLine("Peзультирующая строка: " + str);
+            Console.WriteLine();
+            strOp -= list[0];
+            strOp += list[2];
+            str = "Это простой тест.";
+            strOp(ref str);
+            Console.WriteLine("Peзультирующая строка: " + str);
+            Console.WriteLine("Для завершение работы приложение нажмите клавишу <Enter>");
+            Console.Read();
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    delegate void MyEventHandler();
+    //8 task complete
+    class MyEvent
+    {
+        MyEventHandler[] events = new MyEventHandler[3];
+        public event MyEventHandler SomeEvent
+        {
+            add
+            {
+                int i;
+                for (i = 0; i < 3; i++)
+                {
+                    if (events[i] == null)
+                    {
+                        events[i] = value;
+                        break;
+                    }
+                }
+                if (i == 3) Console.WriteLine("List of handlers is full");
+            }
+            remove
+            {
+                int i;
+                for (i = 0; i < 3; i++)
+                {
+                    if (events[i] == value)
+                    {
+                        events[i] = null;
+                        break;
+                    }
+                }
+                if (i == 3) Console.WriteLine("Event handler not found");
+            }
+        }
+        public void OnSomeEvent()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (events[i] != null) events[i]();
+            }
+        }
+    }
+
+    class W
+    {
+        public void Whandler()
+        {
+            Console.WriteLine("W event was received");
+        }
+    }
+
+    class X
+    {
+        public void Xhandler()
+        {
+            Console.WriteLine("X event was received");
+        }
+    }
+    class Y
+    {
+        public void Yhandler()
+        {
+            Console.WriteLine("Y event was received");
+        }
+    }
+
+    class Z
+    {
+        public void Zhandler()
+        {
+            Console.WriteLine("Z event was received");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "   Пример №2";
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            var ev = new MyEvent();
+            Console.WriteLine("Adding events handlers");
+            var xOb = new X();
+            ev.SomeEvent += new MyEventHandler(new W().Whandler);
+            ev.SomeEvent += new MyEventHandler(xOb.Xhandler);
+            ev.SomeEvent += new MyEventHandler(new Y().Yhandler);
+            ev.SomeEvent += new MyEventHandler(new Z().Zhandler);
+            Console.WriteLine();
+            ev.OnSomeEvent();
+            Console.WriteLine();
+            Console.WriteLine("Deleting handler X");
+            ev.SomeEvent -= new MyEventHandler(xOb.Xhandler);
+            ev.OnSomeEvent();
+            Console.WriteLine();
+            Console.WriteLine("Trying to remove X");
+            ev.SomeEvent -= new MyEventHandler(xOb.Xhandler);
+            ev.OnSomeEvent();
+            Console.WriteLine();
+            Console.WriteLine("Adding handler Z");
+            ev.SomeEvent += new MyEventHandler(new Z().Zhandler);
+            ev.OnSomeEvent();
+            //Console.WriteLine();
+            //ev.SomeEvent -= new MyEventHandler(xOb.Xhandler);
+            //ev.OnSomeEvent();
+            Console.WriteLine("Для завершение работы приложение нажмите клавишу <Enter>");
+            Console.Read();
+        }
+    }
+}
